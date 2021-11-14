bc981ff5461b68508d7c61c0606e10f5713af808
namho kim <skaghzz@naver.com>
2021-11-14 23:23:40 +0900

    ViewModifier

---
+++ class-Memorize/Memorize/Cardify.swift
@@ -0,0 +1,38 @@
+//
+//  Cardify.swift
+//  Memorize
+//
+//  Created by dev on 2021/11/14.
+//
+
+import SwiftUI
+
+struct Cardify: ViewModifier {
+    var isFaceUp: Bool
+    
+    func body(content: Content) -> some View {
+        ZStack {
+            let shape = RoundedRectangle(cornerRadius: DrawingConstants.cornerRadius)
+            if isFaceUp {
+                shape.fill().foregroundColor(.white)
+                shape.strokeBorder(lineWidth: DrawingConstants.lindWidth)
+                content
+            } else {
+                shape.fill()
+            }
+        }
+    }
+    
+    
+    private struct DrawingConstants {
+        static let cornerRadius: CGFloat = 20
+        static let lindWidth: CGFloat = 3
+    }
+}
+
+
+extension View {
+    func cardify(isFaceUp: Bool) -> some View {
+        self.modifier(Cardify(isFaceUp: isFaceUp))
+    }
+}
