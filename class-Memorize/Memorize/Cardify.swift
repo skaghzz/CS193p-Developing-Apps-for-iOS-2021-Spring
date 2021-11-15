f39d70040aa063f286f0e2391d44c386d3c65126
namhokim <skaghzz@naver.com>
2021-11-15 10:03:12 +0900

    complete lec 7

--- class-Memorize/Memorize/Cardify.swift
+++ class-Memorize/Memorize/Cardify.swift
@@ -16,10 +16,11 @@ struct Cardify: ViewModifier {
             if isFaceUp {
                 shape.fill().foregroundColor(.white)
                 shape.strokeBorder(lineWidth: DrawingConstants.lindWidth)
-                content
             } else {
                 shape.fill()
             }
+            content
+                .opacity(isFaceUp ? 1 : 0)
         }
     }
     
